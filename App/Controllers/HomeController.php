610aac55ef1a18409729cf9ace03e0725436d1ef
Realitynauts <realitynauts@bugquest.fr>
2025-04-26 00:12:22 +0200

    Enhance homepage routing and response handling; implement option-based homepage resolution

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -3,12 +3,23 @@
 namespace App\Controllers;
 
 use BugQuest\Framework\Models\Response;
+use BugQuest\Framework\Services\PageService;
 use BugQuest\Framework\Services\View;
 
 class HomeController
 {
     public static function index(): Response
     {
+        $homepage = PageService::getCurrent();
+        if ($homepage) {
+            return Response::view('@app/page.twig',[
+                'page' => PageService::getCurrent(),
+                'body' => PageService::getCurrent()?->parseBlocks(),
+                'style' => PageService::getStyle(),
+                'title' => PageService::getCurrent()?->title,
+                'breadcrumbs' => PageService::getCurrent()?->breadcrumbs(),
+            ]);
+        }
         return Response::view('@app/home.twig');
     }
 }
